f17ffa8ff130f54a04e61741fbbc9ee4aba7cf97
AmmarAlhmoud <ammar.m.alhmoud@gmail.com>
2024-12-10 23:15:22 +0300

    Making it responsive

--- series-locker-app - Copy/src/pages/Logout.jsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "react-router-dom";
-import Cookies from "js-cookie";
-
-export const logoutAction = () => {
-  Cookies.remove("token");
-  Cookies.remove("experation");
-
-  return redirect("/");
-};
